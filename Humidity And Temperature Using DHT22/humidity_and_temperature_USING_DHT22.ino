5ac20eef39eb1be5dfe120f2482d1df57dfb9d48
Apoorv <vapoorve@gmail.com>
2019-04-26 10:31:29 +0530

    Working with Arduino

---
+++ Humidity And Temperature Using DHT22/humidity_and_temperature_USING_DHT22.ino
@@ -0,0 +1,21 @@
+//install DHT sensor library 1.2.3V
+#include <DHT.h>
+
+DHT dht(8,DHT22);
+float humidity;
+float Temerature;
+
+void setup() {
+Serial.begin(9600);
+dht.begin();
+}
+
+void loop() {
+ humidity = dht.readHumidity();
+ Temerature = dht.readTemperature();
+ Serial.print("\n humidity : ");
+ Serial.print(humidity);
+ Serial.print("% ,Temerature : ");
+ Serial.print(Temerature);
+ delay(2000);
+}
